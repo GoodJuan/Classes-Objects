0cb4dc0c0f2b9e157eaed11345d0ec8b5e430be0
GoodJuan <190141@hkis.edu.hk>
2016-09-01 09:52:12 +0800

    init project

--- src/MainCar.java
+++ src/MainCar.java
@@ -1,4 +1,9 @@
 
 public class MainCar {
-
+	
+	Car bugatti = new Car(2700000, "Bugatti Veyron", (float) 408.84, true);
+	bugatti.getInfo();
+	Car mazda = new Car(90000, "Mazda 626", (float) 210, false);
+	mazda.getInfo();
+		
 }
